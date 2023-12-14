99e96273cab59c73afe307a4d9ae185e713ff278
Boot <boot@mac64.local>
2023-12-14 23:53:12 +0530

    removed tinkering

--- src/main/java/dev/spring/REST/RestApplication.java
+++ src/main/java/dev/spring/REST/RestApplication.java
@@ -10,8 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class RestApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(RestApplication.class, args);
-		Arrays.stream(ctx.getBeanDefinitionNames()).forEach(System.out::println);
+		SpringApplication.run(RestApplication.class, args);
 	}
 
 }

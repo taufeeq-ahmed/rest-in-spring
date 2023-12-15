4fe2c35f653f4e846154066e66d44972c0f3e8be
Boot <boot@mac64.local>
2023-12-15 19:00:11 +0530

    CRUD routes are set

--- src/main/java/dev/spring/REST/model/Content.java
+++ src/main/java/dev/spring/REST/model/Content.java
@@ -8,11 +8,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 public record Content(
-                @Id Integer id,
-                @NotEmpty @NotBlank String title,
-                @NotEmpty @NotBlank String desc,
-                @NotEmpty Status status,
-                Type contentType,
-                @NotEmpty LocalDateTime dateCreated,
-                @NotEmpty LocalDateTime dateUpdated) {
+        @Id Integer id,
+        @NotEmpty @NotBlank String title,
+        @NotEmpty @NotBlank String desc,
+        Status status,
+        Type contentType,
+        LocalDateTime dateCreated,
+        LocalDateTime dateUpdated) {
 }

64a3ef01f1067d4f074dfa71a93a1ca0dcba6a41
Boot <boot@mac64.local>
2023-12-15 14:44:14 +0530

    cross origin access added

--- src/main/java/dev/spring/REST/controller/ContentController.java
+++ src/main/java/dev/spring/REST/controller/ContentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import dev.spring.REST.model.Content;
 import dev.spring.REST.repository.ContentCollectionRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/content")
 public class ContentController {
     private final ContentCollectionRepository repository;
